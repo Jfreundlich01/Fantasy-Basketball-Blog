da05879e15007bf18a2696997f90116245c3225b
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-06-10 21:36:17 -0400

    index working, added postBody to postsShema and seed

--- server.js
+++ server.js
@@ -44,9 +44,10 @@ const PlayerSchema = new Schema({
 //makle Posts Schema
 const PostSchema = new Schema({
     firstName: String,
-    lastNAme: String,
-    PostOwner: String,
-    Comments: Array,
+    lastName: String,
+    postOwner: String,
+    postbody: String,
+    comments: Array,
 }, {
     timestamps: true
 })
@@ -92,10 +93,10 @@ app.get("/players/seed", (req, res) => {
 
 //Post Seed
 starterPosts = [
-    {firstName: "Lebron", lastName: "James", PostOwner: "JFREUNDLICH", Comments: []},
-    {firstName: "Steph", lastNAme: "Curry", PostOwner: "JFREUNDLICH", Comments: []},
-    {firstName: "Ja", lastNAme: "Morant", PostOwner: "BigSkipnDemon", Comments: []},
-    {firstName: "James", lastNAme: "Harden", PostOwner: "SaquanBarkely01", Comments: []}
+    {firstName: "Lebron", lastName: "James", postOwner: "JFREUNDLICH", postBody: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",comments: []},
+    {firstName: "Steph", lastName: "Curry", postOwner: "JFREUNDLICH",postBody: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis egestas sed tempus urna et pharetra. Non sodales neque sodales ut etiam. Viverra nam libero justo laoreet sit amet cursus. Dolor magna eget est lorem ipsum dolor. Sed augue lacus viverra vitae congue eu. Habitant morbi tristique senectus et netus et malesuada fames.", comments: []},
+    {firstName: "Ja", lastName: "Morant", postOwner: "BigSkipnDemon",postBody: "In metus vulputate eu scelerisque felis imperdiet. Ornare lectus sit amet est placerat in egestas erat imperdiet. Scelerisque fermentum dui faucibus in. Risus viverra adipiscing at in tellus integer. Quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper. Nam at lectus urna duis convallis. Nisl nunc mi ipsum faucibus vitae. Dictumst vestibulum rhoncus est pellentesque elit.", comments: []},
+    {firstName: "James", lastName: "Harden", postOwner: "SaquanBarkely01",postBody: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Accumsan tortor posuere ac ut. Nec feugiat nisl pretium fusce id velit. Gravida arcu ac tortor dignissim convallis. Ac auctor augue mauris augue. Integer enim neque volutpat ac tincidunt vitae. Enim blandit volutpat maecenas volutpat blandit aliquam etiam.", comments: []}
 ]
 
 app.get("/posts/seed", (req, res) => {
@@ -109,6 +110,24 @@ app.get("/posts/seed", (req, res) => {
       });
     });
   });
+
+//Index
+// index route
+app.get("/index", (req, res) => {
+    // find all the fruits
+    Post.find({})
+      // render a template after they are found
+      .then((posts) => {
+        console.log(posts)
+        res.render("index", {posts});
+      })
+      // send error as json if they aren't
+      .catch((error) => {
+        res.json({ error });
+      });
+  });
+  
+
  
  // new route
 app.get("/new", (req, res) => {
